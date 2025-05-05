b8cc31c588dfa54d1106f553ae2e5f8edf726ff1
Joppe-VH <joppe.vanhoof@gmail.com>
2025-05-05 23:57:50 +0200

    add filters

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -5,8 +5,39 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use ApiPlatform\Metadata\ApiResource;
+use ApiPlatform\Metadata\GetCollection;
+use ApiPlatform\Metadata\Post;
+use ApiPlatform\Metadata\Get;
+use ApiPlatform\Metadata\Patch;
+use ApiPlatform\Metadata\Delete;
+use ApiPlatform\OpenApi\Model\Operation;
+use ApiPlatform\OpenApi\Model\RequestBody;
+use ApiPlatform\Laravel\Eloquent\Filter\PartialSearchFilter;
+use ApiPlatform\Metadata\QueryParameter;
 
-#[ApiResource]
+#[ApiResource(
+    paginationItemsPerPage: 2
+)]
+#[GetCollection()]
+#[Get()]
+#[Patch()]
+#[Delete()]
+#[Post(
+    openapi: new Operation(
+        requestBody: new RequestBody(
+            content: new \ArrayObject([
+                'application/ld+json' => [
+                    'example' => [
+                        'name' => 'Item 1',
+                        'description' => 'Description 1'
+                    ]
+                ]
+            ])
+        )
+    )
+)]
+#[QueryParameter(key: 'name', filter: PartialSearchFilter::class)]
+#[QueryParameter(key: 'description', filter: PartialSearchFilter::class)]
 class Item extends Model
 {
     /** @use HasFactory<\Database\Factories\ItemFactory> */
