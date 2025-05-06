10d3f7c5d9cb6b7d8b154d046b0d8133e882ed91
Joppe-VH <joppe.vanhoof@gmail.com>
2025-05-06 03:09:27 +0200

    make Inventory an api resource

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -2,11 +2,29 @@
 
 namespace App\Models;
 
+use ApiPlatform\Metadata\ApiProperty;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use ApiPlatform\Metadata\ApiResource;
 
+#[ApiResource(
+    paginationItemsPerPage: 10,
+    paginationMaximumItemsPerPage: 30,
+    paginationClientItemsPerPage: true
+)]
 class Inventory extends Model
 {
     /** @use HasFactory<\Database\Factories\InventoryFactory> */
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+    ];
+
+    #[ApiProperty(writable: false)]
+    public function items(): HasMany
+    {
+        return $this->hasMany(Item::class);
+    }
 }
